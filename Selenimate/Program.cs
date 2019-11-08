cb01924a72f832e4a963945f5aa4490a276ab7e2
Majid Ali Khan Quaid <MajidAliKhanQuaid@outlook.com>
2019-11-08 21:39:12 +0500

    Add project files.

---
+++ Selenimate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Selenimate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
